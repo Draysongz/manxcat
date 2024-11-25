eb1033f612c33865139f23e07311375f3f00ee13
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-25 09:58:32 +0100

    done

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,9 @@ import theme from "./components/theme.js";
 import App from "./App.js"
 
 import './index.css';
-import { setupConfig } from "@girishsawant999/react-translate-with-google-api";
 
-setupConfig({
-  clientEmail: import.meta.env.VITE_GCP_CLIENT_SERVICE_ACC_EMAIL,
-  privateKey: import.meta.env.VITE_APP_GCP_PRIVATE_KEY,
-  projectId: import.meta.env.VITE_APP_GCP_PROJECT_ID,
-});
+
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
