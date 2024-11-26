60dc3cd09480a2eae7ec851d65c1307eacd5b557
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-26 11:29:40 +0100

    ddd

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -59,10 +59,8 @@ function UserContext({ children }: { children: ReactNode }) {
   const referralId = params.get("referralId");
 
   useEffect(() => {
-    // const name = WebApp.initDataUnsafe.user?.first_name;
-    // const telegramId = WebApp.initDataUnsafe.user?.id;
-    const name = 'habibilord'
-    const telegramId= 2146305061
+    const name = WebApp.initDataUnsafe.user?.first_name;
+    const telegramId = WebApp.initDataUnsafe.user?.id;
     if (!name || !telegramId) {
       return console.log("Open mini app in telegram");
     }
