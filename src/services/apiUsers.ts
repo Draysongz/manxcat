0a8b4b9d90c47008ef672c34da68b94c043718b2
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-26 11:03:37 +0100

    done

--- src/services/apiUsers.ts
+++ src/services/apiUsers.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { storage } from "../utils/helpers";
 
-const BASE_URL = "https://8a67-105-112-184-143.ngrok-free.app/api/v1/users";
+const BASE_URL = "https://51b2-105-112-184-143.ngrok-free.app/api/v1/users";
 
 export type OwnedCatType = {
   catId: string;
